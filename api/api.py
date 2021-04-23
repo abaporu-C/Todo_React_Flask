3c964707164149e6e7660765000bfa8a55702670
abaporu-C <77457350+abaporu-C@users.noreply.github.com>
2021-04-23 11:54:57 -0400

    Add to do implemented

--- api/api.py
+++ api/api.py
@@ -1,10 +1,36 @@
-from flask import Flask
+from flask import Flask, jsonify, request, json
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+db = SQLAlchemy(app);
 
-@app.route('/', methods=['GET'])
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+
+    def __str__(self):
+        return f'{self.id} {self.content}'
+
+def todo_serializer(todo):
+    return {
+        'id': todo.id,
+        'content': todo.content
+    } 
+
+@app.route('/api', methods=['GET'])
 def index():
-    return {'name':'Hello, World!'}
+    return jsonify([*map(todo_serializer, Todo.query.all())])
+
+@app.route('/api/create', methods=['POST'])
+def create():
+    request_data=json.loads(request.data)
+    todo = Todo(content=request_data['content'])
+
+    db.session.add(todo)
+    db.session.commit()
+
+    return {'201': 'todo created succesfully'}
 
 if __name__ == '__main__':
     app.run(debug=True)
